b54bff6e64adcd798ba97b330bb574b7ea21cbce
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2022-12-11 21:09:12 +0530

    Add all file

---
+++ DSA in java/GCDandLCM.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class GCDandLCM {
+    public static void main(String[] args){
+        try(Scanner scr = new Scanner(System.in)){
+            
+        }
+    }
+}
